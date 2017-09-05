5687a455ba8db8361db22f33c5171ef16657c649
Ashish <ashish.ashish.gupta745@gmail.com>
2017-09-05 15:13:24 +0530

    new

--- app/src/main/java/com/rstintl/docta/deliveryApp/Activities/MainActivity.java
+++ app/src/main/java/com/rstintl/docta/deliveryApp/Activities/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -43,16 +45,37 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.manage, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_manage:
+                Intent intent = new Intent(this, ManageActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_dashboard);
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.content, ItemOneFragment.newInstance());
+        transaction.replace(R.id.content, ItemTwoFragment.newInstance());
         transaction.commit();
         if(getIntent().getExtras() != null){
-            Toast.makeText(getApplicationContext(), getIntent().getStringExtra("assigned_to"),Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getApplicationContext(), getIntent().getStringExtra("assigned_to"),Toast.LENGTH_SHORT).show();
         }
         FloatingActionButton floatingActionButton = (FloatingActionButton)findViewById(R.id.fab);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
